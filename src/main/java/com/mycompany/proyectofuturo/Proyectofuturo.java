39272727d6833ba8ca640fb74a9d535b6dff5631
Borja <borjabmc@icloud.com>
2022-11-21 16:56:35 +0100

    Primer proyecto

---
+++ src/main/java/com/mycompany/proyectofuturo/Proyectofuturo.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.proyectofuturo;
+
+public class Proyectofuturo {
+
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0] + " : ");    }
+    public class veredicto {
+static String veredicto(String  valor1, String valor2) {
+   if (valor1.equals(valor2)) {
+    return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+}
+if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+    return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+}
+return "Ideal. Trabajas los contenidos en casa.";
+}
+}
+}
+
+
+
+
